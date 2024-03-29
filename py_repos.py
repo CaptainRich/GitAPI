58c8cbc73a7d1fbe38a2775b58ec8ade056a76a7
RichA <rich_ay@sbcglobal.net>
2024-03-29 16:39:22 -0500

    More work on viewing repository information.

--- py_repos.py
+++ py_repos.py
@@ -22,9 +22,31 @@ print( f"Complete results: {not response_dict['incomplete_results']}" )
 repo_dicts = response_dict['items']
 print( f"Repositories returned: {len(repo_dicts)}" )
 
+
+""" Depreciated code ...
 # Report on the first repo returned.
 repo_dict = repo_dicts[0]
 print( f"\nKeys: {len(repo_dict)}" ) 
 
 for key in sorted( repo_dict.keys() ):
     print( key )
+
+# Report selected information on this (1st) repo
+print( "\nSelected information from the 1st repository:" )
+print( f"Name: {repo_dict['name']}" )
+print( f"Owner: {repo_dict['owner']['login']}" )
+print( f"Stars: {repo_dict['stargazers_count']}" )
+print( f"Repository: {repo_dict['html_url']}" )
+print( f"Created: {repo_dict['created_at']}" )
+print( f"Updated: {repo_dict['updated_at']}" )
+print( f"Description: {repo_dict['description']}" )
+
+.... end of depreciated code. """
+
+print( "\nSelected information about each returned repository:" )
+for repo_dict in repo_dicts:
+    print( f"\nName: {repo_dict['name']}" )
+    print( f"Owner: {repo_dict['owner']['login']}" )
+    print( f"Stars: {repo_dict['stargazers_count']}" )
+    print( f"Repository: {repo_dict['html_url']}" )
+    print( f"Description: {repo_dict['description']}" )
